b714b18407b01ceb97c9a3aa1da5bb83a4d69877
Jcole429 <Justincole29@hotmail.com>
2020-05-25 17:44:13 -0500

    [development] Added Message model

---
+++ Squad-Chat/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Squad-Chat
+//
+//  Created by Justin Cole on 5/25/20.
+//  Copyright © 2020 Jcole. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
